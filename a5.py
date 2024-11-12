46f7923e001e8c7950b1425d7fdaf62f9e46c1ff
Jovial-10 <jbrana@cps.edu>
2024-11-12 11:05:10 -0600

    update updated

--- a5.py
+++ a5.py
@@ -66,7 +66,7 @@ class Board:
         """Prints all numbers assigned to cells, excluding lists of possible numbers
         that can still be assigned to cells"""
         row_str = ""
-        for i, r in enumerate(self.rows):
+        for i, r in enumerate(self.rows):  #enumerate - 
             if not i % 3:
                 row_str += " -------------------------\n"
 
@@ -106,6 +106,8 @@ class Board:
         Returns:
             a tuple of row, column index identifying the most constrained cell
         """
+
+        #loop through everything and find 
         pass
 
     def failure_test(self) -> bool:
@@ -139,7 +141,18 @@ class Board:
             column - index of the column to assign
             assignment - value to place at given row, column coordinate
         """
-        pass
+
+        for i in range: (self.size)
+
+        remove_if_exists(self.rows[row][i], assignment)
+            # remove the assignment from the column
+        remove_if_exists(self.rows[i][column], assignment)
+
+        print(self.subgrid_coordinates(row, column))
+        for i, j in self.subgrid_coordinates(row, column):
+            remove_if_exists(self.rows[i][j], assignment)
+
+       
 
 
 def DFS(state: Board) -> Board:
@@ -194,36 +207,36 @@ if __name__ == "__main__":
     #     solution.print_pretty()
 
     # # sets of moves for the different games
-    # first_moves = [
-    #     (0, 1, 7),
-    #     (0, 7, 1),
-    #     (1, 2, 9),
-    #     (1, 3, 7),
-    #     (1, 5, 4),
-    #     (1, 6, 2),
-    #     (2, 2, 8),
-    #     (2, 3, 9),
-    #     (2, 6, 3),
-    #     (3, 1, 4),
-    #     (3, 2, 3),
-    #     (3, 4, 6),
-    #     (4, 1, 9),
-    #     (4, 3, 1),
-    #     (4, 5, 8),
-    #     (4, 7, 7),
-    #     (5, 4, 2),
-    #     (5, 6, 1),
-    #     (5, 7, 5),
-    #     (6, 2, 4),
-    #     (6, 5, 5),
-    #     (6, 6, 7),
-    #     (7, 2, 7),
-    #     (7, 3, 4),
-    #     (7, 5, 1),
-    #     (7, 6, 9),
-    #     (8, 1, 3),
-    #     (8, 7, 8),
-    # ]
+    first_moves = [
+        (0, 1, 7),
+        (0, 7, 1),
+        (1, 2, 9),
+        (1, 3, 7),
+        (1, 5, 4),
+        (1, 6, 2),
+        (2, 2, 8),
+        (2, 3, 9),
+        (2, 6, 3),
+        (3, 1, 4),
+        (3, 2, 3),
+        (3, 4, 6),
+        (4, 1, 9),
+        (4, 3, 1),
+        (4, 5, 8),
+        (4, 7, 7),
+        (5, 4, 2),
+        (5, 6, 1),
+        (5, 7, 5),
+        (6, 2, 4),
+        (6, 5, 5),
+        (6, 6, 7),
+        (7, 2, 7),
+        (7, 3, 4),
+        (7, 5, 1),
+        (7, 6, 9),
+        (8, 1, 3),
+        (8, 7, 8),
+    ]
 
     # second_moves = [
     #     (0, 1, 2),
